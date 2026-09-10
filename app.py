4ea7e639f3576072bedde16eb72ceaca65d7d1d5
maliriddhima077-ui <maliriddhima077@gmail.com>
2026-09-10 10:33:28 +0530

    Change server port from 5000 to 7070

--- app.py
+++ app.py
@@ -1050,4 +1050,4 @@ if __name__ == '__main__':
     print(" Principal Login:      admin      / admin123")
     print(" Class Teacher (3-A):  teacher3a  / teach123")
     print("==================================================================")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=7070)
